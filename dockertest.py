e445dccc6b95bd4b3834783384c986e41589ef53
Joan Glendinning <41628418+Sinnamoon@users.noreply.github.com>
2023-01-11 16:42:28 +0100

    Update dockertest.py

--- dockertest.py
+++ dockertest.py
@@ -4,11 +4,18 @@ class SimpleMath:
     @staticmethod
     def addition(x, y):
         return x + y
+    @staticmethod
+    def soustraction(x,y):
+        return x - y
+        
 
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         result = SimpleMath.addition(1, 2)
         self.assertEqual(result, 3)
+    def test_sub(self):
+        result = SimpleMath.soustraction(1, 2)
+        self.assertEqual(result, -1)
 
 if __name__ == '__main__':
     unittest.main()
